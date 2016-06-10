f85fe8cc862ab41a158d9004e6fa6010713eebba
James <jamesarmandobazan@gmail.com>
2016-06-09 21:26:20 -0500

    My Additions of HTML and Wikipedia JS

---
+++ js/wikipedia.js
@@ -0,0 +1,15 @@
+	function WD(item) {
+	    url = "http://en.wikipedia.org/w/api.php?action=query&prop=description&titles=" + item.toString() + "&prop=extracts&exintro&explaintext&format=json&redirects&callback=?";
+	    $.getJSON(url, function (json) {
+	    	console.log(json);
+	        var item_id = Object.keys(json.query.pages)[0];
+	        userGiven = json.query.pages[item_id].extract;
+	        result = "<b></b> <t>" + item + "</t> <b> : </b>" + userGiven;
+	        $('#wikipediaObject').append("<div>"+result+"</div>");
+	    });
+	}
+	$(document).on("click", "#addInput", function() {
+		$('#wikipediaObject').html('');
+		var whatIsTyped = $('#userInputText').val();
+		WD(whatIsTyped);
+	});
