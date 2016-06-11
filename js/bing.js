dc07d7878e1f7dfeb0f25dac18fac5d5cf7791d7
Jack Walker <jackw2050@gmail.com>
2016-06-11 11:23:31 -0500

    Fixed concert info formatting

--- js/bing.js
+++ js/bing.js
@@ -41,48 +41,31 @@ function GetNews(bandSelected) {
 }
 
 function GetConcertInfo(bandSelected) {
- new BIT.Widget({ 
-        "artist":"Selena Gomez", 
-        "div_id":"tour-dates",
+
+    new BIT.Widget({
+        "artist": bandSelected,
+        "div_id": "tour-dates",
         "force_narrow_layout": "true",
         "display_limit": "5",
-        "bg_color": "#FFFFFF"
-      }).insert_events();
+        "text_color": "#FFFFFF", 
+        "bg_color": "#003459",
+        "width": "265px",
+        "notify_me": "true"
+    }).insert_events();
 }
 
-/*    $("#show-widget").click(function() { 
-      new BIT.Widget({ 
-        "artist":"Selena Gomez", 
-        "div_id":"tour-dates",
-        "force_narrow_layout": "true",
-        "display_limit": "5",
-        "bg_color": "#FFFFFF"
-      }).insert_events();
-    });
-*/
-//http://api.bandsintown.com/artists/Crystal%20Castles.json?app_id=YOUR_APP_ID&api_version=2.0&callback=showArtist
+function myfunction() {
+    var queryURL = "http://api.bandsintown.com/artists/" + jetTrimmed + ".json?app_id=YOUR_APP_ID&api_version=2.0&callback=showArtist";
 
-
-    var params2 = {
-        // Request parameters
-        "q": "selena gomez"
- 
-    };
     $.ajax({
-            url: "http://api.bandsintown.com/artists/selena gomez.json?app_id=YOUR_APP_ID&api_version=2.0&callback=showArtist"  ,
-            beforeSend: function(xhrObj) {
-                // Request headers
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "9dd9941d21e24c4d921bf0532dced130");
-            },
-            type: "GET",
-            // Request body
-            data: "{body}",
-        })
-        .done(function(data) {
-           
-            console.log(data);
-            // alert("success");
-        })
-        .fail(function() {
-            alert("error");
-        });
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response) {
+        console.log(response);
+
+    });
+
+}
+
+
+//url: "http://api.bandsintown.com/artists/selena gomez.json?app_id=YOUR_APP_ID&api_version=2.0&callback=showArtist",
